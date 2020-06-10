7f1e28f69015341fd9d58df42916cd2876f1248e
1635337 <66416744+1635337@users.noreply.github.com>
2020-06-10 12:56:45 +0300

    Add project files.

---
+++ 1635337 first loop priject/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace _1635337_first_loop_priject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.TxtUser = new System.Windows.Forms.Label();
+            this.TxtPassword = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.Location = new System.Drawing.Point(375, 57);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(93, 40);
+            this.BtnLogin.TabIndex = 0;
+            this.BtnLogin.Text = "LOGIN";
+            this.BtnLogin.UseVisualStyleBackColor = true;
+            this.BtnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(375, 127);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 41);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "CLOSE";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.TxtPassword);
+            this.groupBox1.Controls.Add(this.TxtUser);
+            this.groupBox1.Location = new System.Drawing.Point(70, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(244, 145);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login to IE322";
+            // 
+            // TxtUser
+            // 
+            this.TxtUser.AutoSize = true;
+            this.TxtUser.Location = new System.Drawing.Point(23, 34);
+            this.TxtUser.Name = "TxtUser";
+            this.TxtUser.Size = new System.Drawing.Size(69, 17);
+            this.TxtUser.TabIndex = 0;
+            this.TxtUser.Text = "Username";
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.AutoSize = true;
+            this.TxtPassword.Location = new System.Drawing.Point(23, 91);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.Size = new System.Drawing.Size(66, 17);
+            this.TxtPassword.TabIndex = 1;
+            this.TxtPassword.Text = "Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 24);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 91);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 24);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 225);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.BtnLogin);
+            this.Name = "Form1";
+            this.Text = "My Program";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnLogin;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label TxtPassword;
+        private System.Windows.Forms.Label TxtUser;
+    }
+}
+
---
+++ 1635337 first loop priject/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1635337_first_loop_priject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            string username = "Mo";
+            string mypassword = "12345";
+
+            int attempt = 1;
+            int maxattempts = 3;
+
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void label1_Click(object sender, EventArgs e)
+            {
+
+            }
+
+            private void label2_Click(object sender, EventArgs e)
+            {
+
+            }
+
+            private void BtnLogin_Click(object sender, EventArgs e)
+            {
+
+                while (attempt <= maxattempts)
+                {
+                    if (TxtUser.Text != username)
+
+
+
+                }
+
+                else
+                {  // username is correct
+                    // so check password
+                    if (TxtPassword.Text != "12345")
+                    {
+                        attempt++;
+                        // incorrect password attempt++;
+                        Message
+
+                { // username is incorrect
+                            MessageBox.Show("invalid username," + (maxattempts - attempt) +
+                                "attempts remaining");
+                            TxtUser.Clear();
+
+                            attempt++;
+                            return;
+                            Box.Show("incorrect password" + (maxattempts - attempt) +
+                                                        "attempts remaining");
+                            TxtPassword.Clear();
+
+                            return;
+                        }
+
+                    else
+
+
+                        {
+                            // both are correct
+                            attempt = 0; // reset the number of attempts
+                            MessageBox.Show("Login Successfull");
+                            BtnLogin.Text =
+                                "Logout";
+
+
+                        }
+                    }
+                }
+            }
